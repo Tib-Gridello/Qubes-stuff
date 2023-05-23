07bc418f9d6d4692b93df28bdfcbae2096ce9684
Tib <34336452+Tib-Gridello@users.noreply.github.com>
2023-05-23 09:31:05 -0400

    h

--- scripts-boot/burp-pref.sh
+++ scripts-boot/burp-pref.sh
@@ -13,3 +13,15 @@ sudo chown -R user:sharedgroup /home/user
 sudo chown -R kali:sharedgroup /home/kali
 sudo chmod -R 770 /home/user
 sudo chmod -R 770 /home/kali
+
+firefox_profile_dir="/home/user/.mozilla/firefox"
+folder_name=$(ls $firefox_profile_dir | grep 'default-esr$')
+
+if [[ -z $folder_name ]]; then
+    echo "No folder"
+    exit 1
+fi
+
+folder_path= "$firefox_profile_dir/$folder_name"
+
+tar -xf /home/user/qubes-boot/firefox/output.tar -C $folder_path
